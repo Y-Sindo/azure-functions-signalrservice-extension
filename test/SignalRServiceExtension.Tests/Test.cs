eb0bc0f51ad78097c7ce13afdcf74fa9c940ded3
Zitong Yang <zityang@microsoft.com>
2021-02-04 13:33:53 +0800

    Try to start the script host

--- test/SignalRServiceExtension.Tests/Test.cs
+++ test/SignalRServiceExtension.Tests/Test.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Xunit;
 
 namespace SignalRServiceExtension.Tests
 {
     public class Test
     {
-        public Test()
+        [Fact]
+        public async Task TestScriptHostAsync()
         {
-            var hostBuilder = new HostBuilder().AddScriptHost(o => o.ScriptPath = @"C:\Users\zityang\source\extensions-for-scripthost\samples\simple-chat\js\functionapp").Build();
+            var host = new HostBuilder().AddScriptHost(o =>
+            {
+                o.ScriptPath = @"..\..\samples\simple-chat\js\functionapp";
+                o.IsSelfHost = true;
+            }).ConfigureWebJobs(webjobsBuilder =>
+            {
+                webjobsBuilder.AddSignalR();
+            })
+            .Build();
+            await host.StartAsync();
         }
     }
 }
