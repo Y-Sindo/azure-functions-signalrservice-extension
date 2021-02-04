4517e2adc409d990537abbabba19261550f071f2
Zitong Yang <zityang@microsoft.com>
2021-02-04 11:32:10 +0800

    Try add script host

---
+++ test/SignalRServiceExtension.Tests/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Extensions.Hosting;
+
+namespace SignalRServiceExtension.Tests
+{
+    public class Test
+    {
+        public Test()
+        {
+            var hostBuilder = new HostBuilder().AddScriptHost(o => o.ScriptPath = @"C:\Users\zityang\source\extensions-for-scripthost\samples\simple-chat\js\functionapp").Build();
+        }
+    }
+}
